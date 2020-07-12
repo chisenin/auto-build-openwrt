a330909c20a1f24ba5b3aa951237ba8a23e1e9cd
chisenin <65078969+chisenin@users.noreply.github.com>
2020-07-13 07:19:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,10 +9,11 @@
 #=============================================================
 
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-# 添加科学上网和依赖库 | 2020/7/3 by chisenin
-sed -i '$a src-git luci-app-adguardhome https://github.com/rufengsuixing/luci-app-adguardhome' feeds.conf.default
-# sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+
+# 添加第三方软件包 | 2020/7/3 by chisenin
+git clone  https://github.com/rufengsuixing/luci-app-adguardhome luci-app-adguardhome
+git clone  https://github.com/fw876/helloworld  helloworld
