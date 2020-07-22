57a878d860ae88f9db3f612870ee2797c4fd1d85
chisenin <65078969+chisenin@users.noreply.github.com>
2020-07-22 13:17:01 +0800

    试图使用SVN命令失败，换回git命令

--- diy-lede1.sh
+++ diy-lede1.sh
@@ -15,6 +15,7 @@
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 # 添加adguardhome和ssrPLUS+ | 2020/7/22 by chisenin
-svn co https://github.com/kenzok8/openwrt-packages/tree/master/adguardhome ./package/adguardhome
-svn co https://github.com/kenzok8/openwrt-packages/tree/master/luci-app-adguardhome ./package/luci-app-adguardhome
-svn co https://github.com/kenzok8/openwrt-packages/tree/master/luci-app-ssr-plus ./package/luci-app-ssr-plus
+# svn co https://github.com/kenzok8/openwrt-packages/tree/master/adguardhome ./package/adguardhome
+# svn co https://github.com/kenzok8/openwrt-packages/tree/master/luci-app-adguardhome ./package/luci-app-adguardhome
+# svn co https://github.com/kenzok8/openwrt-packages/tree/master/luci-app-ssr-plus ./package/luci-app-ssr-plus
+git clone https://github.com/kenzok8/openwrt-packages.git ./package/packages
